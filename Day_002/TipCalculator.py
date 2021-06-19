3f721a70054e55754a4a518e597bb68d4c4c5bcd
Ioannis Katsaros <katsarosia@gmail.com>
2021-06-19 16:48:37 +0200

    Day 002 - change order of input

--- Day_002/TipCalculator.py
+++ Day_002/TipCalculator.py
@@ -1,8 +1,8 @@
 def main():
     print("Welcome to TipCalculator 3000, where the numbers are made up and the currency don't matter.")
     bill = float(input("What was the total bill? "))
-    people = int(input("How many people to split the bill? "))
     tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+    people = int(input("How many people to split the bill? "))
     dirtyBillPerPerson = (bill * (100 + tip) / 100) / people
     billPerPerson = int(dirtyBillPerPerson * 100) / 100
     print(f'Each person should pay: {billPerPerson} ')
