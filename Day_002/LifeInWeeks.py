9ac2fd206b8eb366f9c89e4c8ef885948eafe4fb
Ioannis Katsaros <katsarosia@gmail.com>
2021-06-19 16:40:21 +0200

    Day 002 - Had a little too much fun

---
+++ Day_002/LifeInWeeks.py
@@ -0,0 +1,25 @@
+import time
+
+def main():
+    print("Hello there and welcome to Depresso3000, the bot that fills you with angst and despair")
+    time.sleep(3)
+    print("Today Depresso3000 wants to talk about the brevity of your life.")
+    time.sleep(3)
+    print("""Are you ready to feel meaningless and at the same time be crushed with the 
+sudden responsability of your entire future as you realize the inevitable march of time
+pushes you every second closer to your death?""")
+    time.sleep(9)
+    print("Depresso3000 doesn't care and will hurt you despite your pleas of grace.")
+    time.sleep(3)
+    print("Get rekt meatbag!")
+    time.sleep(2)
+    age = int(input("How old are you in earth years, gregorian calendar?"))
+    weeksLeftToLive = (90 - age) * 365 / 7
+    print(f"""Depresso3000 estimates that if you're lucky enough to live to be 90 one day,
+you still have {weeksLeftToLive} weeks, not counting leap days, nor the weeks since
+your last birthday""")
+    time.sleep(9)
+    print("Depresso3000 wishes you a great panic attack and is thrilled by your display of terror")
+
+if __name__ == "__main__":
+    main()
