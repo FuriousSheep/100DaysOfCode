303927483cb449b0fabbe28f4a1df419e84300ac
Ioannis Katsaros <katsarosia@gmail.com>
2021-06-20 06:43:17 +0200

    Day 003 - Am I really making isOdd? Why yes, yes I am

---
+++ Day_003/isOdd.py
@@ -0,0 +1,9 @@
+def main():
+    number = int(input("Please enter a number so the great Nostradamus may determine if it's even or odd: "))
+    if number % 2 :
+        print("THE NUMBER IS ODD")
+    else:
+        print("THE NUMBER IS NOT ODD. IN FACT, IT IS VERY NORMAL. SO NORMAL, WE MIGHT CALL IT EVEN.")
+
+if __name__ == "__main__":
+    main()
