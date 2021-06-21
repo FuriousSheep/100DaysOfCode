deee289acfb2621ec12b2df9c4d5af9d41b6bc03
FuriousSheep <46798566+FuriousSheep@users.noreply.github.com>
2021-06-21 15:07:40 +0200

    Day 006 - keep to your right

---
+++ Day_006/Maze.py
@@ -0,0 +1,16 @@
+# https://reeborg.ca/reeborg.html Maze
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+    else:
+        turn_right()
+        move()
