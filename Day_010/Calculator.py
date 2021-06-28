787719795a659b0fb76e379666d8baa349540241
Ioannis Katsaros <katsarosia@gmail.com>
2021-06-28 17:52:43 +0200

    Day 010 - Refactor Calculator Alligator

--- Day_010/Calculator.py
+++ Day_010/Calculator.py
@@ -1,26 +1,42 @@
 import CalculatorData as C
 
+
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if not b == 0:
+        return a / b
+    else:
+        print(C.zeroDivisionError)
+        return "ERROR"
+def mod(a, b):
+    if not b == 0:
+        return a % b
+    else:
+        print(C.zeroDivisionError)
+        return "ERROR"
+
+calcDic = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div,
+    "%": mod
+}
+
+
 def Calc(data):
-    if data['op'] == "+":
-        data['res'] = data['n1'] + data['n2']
-    elif data['op'] == "-":
-        data['res'] = data['n1'] - data['n2']
-    elif data['op'] == "*":
-        data['res'] = data['n1'] * data['n2']
-    elif data['op'] == "/":
-        if not data['n2'] == 0:
-            data['res'] = data['n1'] / data['n2']
-        else:
-            print(C.zeroDivisionError)
-    elif data['op'] == "%":
-        if not data['n2'] == 0:
-            data['res'] = data['n1'] % data['n2']
-        else:
-            print(C.zeroDivisionError)
+    #return the appropriate result for the given operator
+    data["res"] = calcDic[data['op']](data['n1'], data['n2'])
     print(f"{data['n1']} {data['op']} {data['n2']} = {data['res']}")
     return data
 
 def startCalc(data):
+    """Get the data needed for the first calculation"""
     data['n1'] = float(input(C.askNumber1Message))
     data['op'] = input(C.askOperatorMessage)
     data['n2'] = float(input(C.askNumber2Message))
@@ -30,7 +46,7 @@ def main():
     print(C.pic)
     data = C.initData   
     data = startCalc(data)
-    while (input(C.continueOperations) == "y"):
+    while (not data['res'] == "ERROR") and (input(C.continueOperations) == "y") :
         temp = data["res"]
         data = C.initData
         data["n1"] = temp
