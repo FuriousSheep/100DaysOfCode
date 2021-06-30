7e50bec9b7dbd0bfbea47ef143154bc57cd6ae6b
Ioannis Katsaros <katsarosia@gmail.com>
2021-07-01 00:55:18 +0200

    Day 014 - HigherLower v1.0

--- Day_014/HigherLower.py
+++ Day_014/HigherLower.py
@@ -6,24 +6,31 @@ from os import system
 def main():
     score = 0
     gameOn = True
+    a = 0
     while gameOn:
         system("clear")
         print(L.logo)
         if score > 0:
             print(f"You're right! Current score: {score}")
-        a = R.choice(D.data)
+        if a == 0:
+            a = R.choice(D.data)
         print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
         D.data.remove(a)
-
-        print(len(D.data))
+        print(L.vs)
         b = R.choice(D.data)
         print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}")
         answer = input("Who has more followers? Type (A) or (B)")
-        gameOn = False
-        # b = R.choice()
-        # # print(f"Compare A:")
-        # print(a)
-        # print(b)
+        solution = "A" if a["follower_count"]>b["follower_count"] else "B"
+        if answer == solution:
+            score += 1
+            a = b
+            print("correct!")
+        else:
+            system("clear")
+            print(L.logo)
+            print(f"Wrong answer. Final score: {score}")
+            gameOn = False
 
+        
 if __name__ == "__main__":
     main()
