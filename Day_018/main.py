521556586755f36cd234e1806bb0851ecd28cef4
Ioannis Katsaros <katsarosia@gmail.com>
2021-07-17 10:15:03 +0200

    Day 018 - RandomColor

--- Day_018/main.py
+++ Day_018/main.py
@@ -1,13 +1,23 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
+from random import randint, choice
+
+
+def random_color():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
 
 
 def main():
     timmy = Turtle()
     timmy.shape("turtle")
-    timmy.color("#00FF00")
-    for i in range(0, 4):
-        timmy.forward(100)
-        timmy.right(90)
+    timmy.speed(0)
+    timmy.pensize(20)
+    colormode(255)
+    timmy.color(random_color())
+    for _ in range(0, 100):
+        timmy.forward(20)
+        timmy.right(choice([0, 90, 180, 270]))
+        timmy.color(random_color())
+
     screen = Screen()
     screen.exitonclick()
 
