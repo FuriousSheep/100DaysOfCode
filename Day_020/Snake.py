516292283ea4873f3bd04ea18ccd0a4b55c30407
Ioannis Katsaros <katsarosia@gmail.com>
2021-07-18 14:37:07 +0200

    Day 018 - Snake progression

--- Day_020/Snake.py
+++ Day_020/Snake.py
@@ -1,10 +1,16 @@
 import turtle as t
 import random as r
+import time
 
 CASE_SIZE = 20
 screen = t.Screen()
 screen.bgcolor("#232323")
-screen.listen()
+screen.setup(width=600, height=600)
+screen.title("Snake! The Game!")
+screen.tracer(0)
+# screen.listen()
+
+#### TEXT ####
 
 
 def setup_score():
@@ -34,23 +40,65 @@ def update_score(score, points):
     score.write(f"Score : {points}", align="center", font=("Arial Black", 20))
 
 
+#### SNAKE CONTROL ####
+
+
+def make_snake_part():
+    snake_part = t.Turtle()
+    snake_part.shape("square")
+    # snake_part.speed(0)
+    snake_part.penup()
+    snake_part.color("#FAFAFA", "#232323")
+    return snake_part
+
+
 def make_snake():
     snake = []
     for i in range(0, 3):
-        snake_part = t.Turtle()
-        snake_part.shape("square")
-        snake_part.speed(0)
-        snake_part.penup()
-        snake_part.color("#FAFAFA", "#232323")
+        snake_part = make_snake_part()
         snake_part.setx(snake_part.xcor() - CASE_SIZE * i)
         snake.append(snake_part)
     return snake
 
 
+def face_north(snake_part):
+    snake_part.setheading(90)
+
+
+def face_south(snake_part):
+    snake_part.setheading(270)
+
+
+def face_east(snake_part):
+    snake_part.setheading(0)
+
+
+def face_west(snake_part):
+    snake_part.setheading(180)
+
+
+def move_snake_part(snake_part):
+    snake_part.forward(20)
+
+
+def move_snake(snake):
+    for snake_part in snake:
+        move_snake_part(snake_part)
+
+#### MAIN ####
+
+
 def main():
-    score = setup_score()
-    make_snake()
+    # score = setup_score()
+    snake = make_snake()
+    screen.update()
+    while True:
+        move_snake(snake)
+        screen.update()
+        time.sleep(0.5)
+
     # game_lost()
+
     screen.exitonclick()
 
 
---
+++ Day_020/Snake_class.py
@@ -0,0 +1,6 @@
+import turtle as T
+
+
+class Snake_class:
+    def __init__(self):
+        pass
