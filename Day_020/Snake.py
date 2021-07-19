5f84841790fa11f44d36e11ac66c2bf961f94cf4
Ioannis Katsaros <katsarosia@gmail.com>
2021-07-19 14:43:09 +0200

    Day 20 - Add Snake class, controls, collision, out-of-bounds control

--- Day_020/Snake.py
+++ Day_020/Snake.py
@@ -1,19 +1,19 @@
 import turtle as t
 import random as r
 import time
+import Snake_class as S
 
-CASE_SIZE = 20
 screen = t.Screen()
 screen.bgcolor("#232323")
 screen.setup(width=600, height=600)
 screen.title("Snake! The Game!")
 screen.tracer(0)
-# screen.listen()
 
 #### TEXT ####
 
 
 def setup_score():
+    """Writes up the initial score on the screen"""
     score = t.Turtle()
     score.penup()
     score.hideturtle()
@@ -24,6 +24,7 @@ def setup_score():
 
 
 def game_lost():
+    """Writes up that the game is lost on the screen"""
     lose = t.Turtle()
     lose.penup()
     lose.color("#FAFAFA")
@@ -36,6 +37,7 @@ def game_lost():
 
 
 def update_score(score, points):
+    """Changes the score writing to show the updated score"""
     score.clear()
     score.write(f"Score : {points}", align="center", font=("Arial Black", 20))
 
@@ -43,59 +45,16 @@ def update_score(score, points):
 #### SNAKE CONTROL ####
 
 
-def make_snake_part():
-    snake_part = t.Turtle()
-    snake_part.shape("square")
-    # snake_part.speed(0)
-    snake_part.penup()
-    snake_part.color("#FAFAFA", "#232323")
-    return snake_part
-
-
-def make_snake():
-    snake = []
-    for i in range(0, 3):
-        snake_part = make_snake_part()
-        snake_part.setx(snake_part.xcor() - CASE_SIZE * i)
-        snake.append(snake_part)
-    return snake
-
-
-def face_north(snake_part):
-    snake_part.setheading(90)
-
-
-def face_south(snake_part):
-    snake_part.setheading(270)
-
-
-def face_east(snake_part):
-    snake_part.setheading(0)
-
-
-def face_west(snake_part):
-    snake_part.setheading(180)
-
-
-def move_snake_part(snake_part):
-    snake_part.forward(20)
-
-
-def move_snake(snake):
-    for snake_part in snake:
-        move_snake_part(snake_part)
-
 #### MAIN ####
-
-
 def main():
     # score = setup_score()
-    snake = make_snake()
+    snake = S.Snake()
+    snake.set_listeners(screen)
     screen.update()
-    while True:
-        move_snake(snake)
+    while not snake.is_dead():
+        snake.move()
         screen.update()
-        time.sleep(0.5)
+        time.sleep(0.15)
 
     # game_lost()
 
